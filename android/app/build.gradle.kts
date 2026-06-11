3bbbb01fb6f56a9245cb17f6328416ab6330abe6
doumil <93338229+doumil@users.noreply.github.com>
2026-06-11 14:25:08 +0100

    update 9 pubyaml for ios

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -4,7 +4,6 @@ import java.io.FileInputStream
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -16,24 +15,20 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.pharmahealthexpo"
-    compileSdkVersion(34)
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.pharmahealthexpo"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -51,7 +46,6 @@ android {
 
     buildTypes {
         release {
-            // Updated to enable both code and resource shrinking
             isMinifyEnabled = true
             isShrinkResources = true
 
@@ -66,4 +60,15 @@ android {
 
 flutter {
     source = "../.."
+}
+
+// 🛠️ التعديل هنا: رد بالك للـ "s" صغيرة باش يقبلها الـ Gradle Compiler
+subprojects {
+    if (project.name == "google_maps_flutter_android") {
+        project.extensions.findByType<org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension>()?.apply {
+            target.compilations.all {
+                kotlinOptions.freeCompilerArgs += listOf("-Xskip-prerelease-check")
+            }
+        }
+    }
 }
